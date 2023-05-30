fffcf3f16323358d3eba23535a5380af3a3f83c5
MehmetErenTuranboy <mehmet.turanboy@gmail.com>
2023-05-31 01:05:57 +0200

    feat: add consumptions and generations

--- packages/worker-example/src/main.ts
+++ packages/worker-example/src/main.ts
@@ -3,6 +3,16 @@ import { appConfig } from './config/app-config'
 import { NestFactory } from '@nestjs/core'
 import { Worker } from '@energyweb/worker'
 import { AppModule } from './app.module'
+import { MatchingModule, MatchingInMemoryModule } from './matching/matching.module'
+import { InputModule, InputInMemoryModule } from './input/input.module'
+import { matchingResultBackendSender } from './matching/receivers/matching-result-backend.receiver'
+import {ProportionalMatcher } from '@energyweb/algorithms'
+import {Entity, EntityConsumption, EntityGeneration, MatchData, Match, MatchPath, MatchRoundInput, PathStrategy } from '@energyweb/algorithms/src/proportional-matcher/types'
+import { EnergyType } from 'types'
+import { PinoLogger } from 'nestjs-pino';
+import { formatBytes32String } from 'ethers/lib/utils'
+import { BytesLike } from "@ethersproject/bytes"; // Assuming you're using the ethers.js library
+import { ethers } from 'ethers';
 
 const { workerBlockchainAddress, ...workerConfig } = appConfig.workerConfig
 const worker = new Worker(workerConfig)
@@ -10,6 +20,68 @@ const worker = new Worker(workerConfig)
 worker.start(async ({ merkleTree, getVotingContract }) => {
   const workerAddress = appConfig.workerConfig.workerBlockchainAddress
   const votingContract = getVotingContract()
+  
+  const consumers: EntityConsumption[] = [
+    {
+      energyPriorities: [
+        { energyType: "electric", priority: 1 },
+        { energyType: "EnergyType.Gas", priority: 2 }
+      ],
+      shouldMatchByRegion: false,
+      shouldMatchByCountry: false,
+      shouldMatchByOtherCountries: false,
+      id: '2',
+      volume: 10,
+      siteId: '3',
+      regionId: '1',
+      countryId: '2'
+    },
+    {
+      energyPriorities: [
+        { energyType: "EnergyType.Water", priority: 1 },
+        { energyType: "oil", priority: 2 }
+      ],
+      shouldMatchByRegion: false,
+      shouldMatchByCountry: true,
+      shouldMatchByOtherCountries: false,
+      id: '1',
+      volume: 20,
+      siteId: '2',
+      regionId: '1',
+      countryId: '1'
+    }
+  ];
+  //printing consumers
+  console.log("Consumers: \n",consumers);
+
+  // Generation values
+  const generations: EntityGeneration[] = 
+  [
+    {
+      id: '3',
+      volume: 60,
+      siteId: 'site6',
+      regionId: 'region6',
+      energyType: 'electric',
+      countryId: '2'
+    },
+    {
+      id: '4',
+      volume: 60,
+      siteId: 'site5',
+      regionId: 'region5',
+      energyType: 'oil',
+      countryId: '1'
+    },
+  ]
+  // Showing generation information
+  console.log("Generations: \n", generations);
+
+  const input: ProportionalMatcher.Input = {
+    consumptions: consumers,
+    generations: generations
+  };
+  console.log("Inputs: \n", input);
 
   console.log(`Worker: ${workerAddress} not registered. Registering...`)
   const tx = await votingContract.addWorker(workerAddress, {
