651b7d1beec3b7d68bde24fc13ff9451444c1576
MehmetErenTuranboy <mehmet.turanboy@gmail.com>
2023-05-31 01:31:18 +0200

    feat: add blockchain connection generation

--- packages/worker-example/src/main.ts
+++ packages/worker-example/src/main.ts
@@ -14,13 +14,38 @@ import { formatBytes32String } from 'ethers/lib/utils'
 import { BytesLike } from "@ethersproject/bytes"; // Assuming you're using the ethers.js library
 import { ethers } from 'ethers';
 
-const { workerBlockchainAddress, ...workerConfig } = appConfig.workerConfig
-const worker = new Worker(workerConfig)
+
+
+const { workerBlockchainAddress, ...workerConfig } = appConfig.workerConfig;
+const worker = new Worker(workerConfig);
+const logger = new PinoLogger({ renameContext: 'matchingResultVotingContractSender' });
+
+async function stringToBytesLike(input: string): Promise<BytesLike> {
+  const encoder = new TextEncoder();
+  const encodedBytes = encoder.encode(input);
+  const bytesLike: BytesLike = Uint8Array.from(encodedBytes);
+  return bytesLike;
+}
+
+type PromiseOrValue<T> = T | Promise<T>;
+
+function convertStringToPromiseOrValue(input: string, shouldReturnPromise: boolean): PromiseOrValue<string> {
+  if (shouldReturnPromise) {
+    return new Promise((resolve) => {
+      resolve(input);
+    });
+  } else {
+    return input;
+  }
+}
+
 
 worker.start(async ({ merkleTree, getVotingContract }) => {
+  
   const workerAddress = appConfig.workerConfig.workerBlockchainAddress
   const votingContract = getVotingContract()
-  
+
+  // Consumer values
   const consumers: EntityConsumption[] = [
     {
       energyPriorities: [
@@ -83,12 +108,14 @@ worker.start(async ({ merkleTree, getVotingContract }) => {
   };
   console.log("Inputs: \n", input);
 
-
   const match: ProportionalMatcher.Result = await ProportionalMatcher.match(input);
 
   console.log("match result", match);
   console.log(`Worker: ${workerAddress} not registered. Registering...`)
-
+  const tx = await votingContract.addWorker(workerAddress, {
+    gasLimit: 1000000,
+  })
+  await tx.wait()
   console.log(`Worker: ${workerAddress} registered`)
 
 
@@ -100,16 +127,38 @@ worker.start(async ({ merkleTree, getVotingContract }) => {
 
   const inputHash = merkleTree.hash(matchingRootHash);
 
-  console.log("inputHash: " + inputHash);
+  
+
+  const tree = merkleTree.createMerkleTree(["leaves", "nodes", "las"]);
+
+  const shouldReturnPromise = true; // Set to true for a Promise, or false for a value
+
+  const result = convertStringToPromiseOrValue(inputHash + "", shouldReturnPromise);
+
+  const inputHashBytes = ethers.utils.arrayify(inputHash);
+  const resultHash = matchingRootHash; // Or any other value that represents the resultHash
+  const resultHashBytes = ethers.utils.arrayify(resultHash);
+  
+  const votingID = await ethers.utils.formatBytes32String("input"); // Or any other input
+  const hashedMatchingRootHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(matchingRootHash));
+  const matchResult = ethers.utils.arrayify(hashedMatchingRootHash);
+  
+  
+  try {
+      const tx = await votingContract.vote(votingID, matchResult, { gasLimit: 1000000, from: workerAddress });
+      console.log("Transaction:", tx);
+  } catch (error) {
+      console.error("Error in voting:", error);
+  }
 
 
-  console.log(`Worker: ${workerAddress} not registered. Registering...`)
-  const tx = await votingContract.addWorker(workerAddress, {
-    gasLimit: 1000000,
-  })
-  await tx.wait()
-  console.log(`Worker: ${workerAddress} registered`)
+  const rootHash = tree.getHexRoot();
+  
+  console.log("wokers output for verification result:", tree.verify(tree.getProof(tree.getLeaf(2)) , tree.getLeaf(2), tree.getRoot()))
+  
 
+  console.log('root hash: ' + rootHash);
+  
   const app = await NestFactory.createApplicationContext(
     AppModule.register({
       merkleTree,
